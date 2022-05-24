34c750cf981552ad84a1b23b693f930e7c489104
fysh711426 <fysh711426@hotmail.com>
2022-05-24 10:47:55 +0800

    init

---
+++ MP3catSharp/MP3cat.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MP3libSharp;
+
+namespace MP3catSharp
+{
+	public class MP3cat
+	{
+		// Create a new file at [outpath] containing the merged contents of the list of input files.
+		public static void merge(string outpath, string tagpath, string[] inpaths, bool force, bool quiet)
+		{
+			var totalFrames = (UInt32)0;
+			var totalBytes = (UInt32)0;
+			var totalFiles = 0;
+			var firstBitRate = 0;
+			var isVBR = false;
+
+			// Only overwrite an existing file if the --force flag has been used.
+			if (!force)
+			{
+				if (File.Exists(outpath))
+					throw new Exception($"Error: the file '{outpath}' already exists.");
+			}
+
+			// If the list of input files includes the output file we'll end up in an infinite loop.
+			foreach (var filepath in inpaths)
+			{
+				if (filepath == outpath)
+					throw new Exception("Error: the list of input files includes the output file.");
+			}
+
+			// Create the output file.
+			using (var outfile = new FileStream(outpath, FileMode.Create, FileAccess.Write))
+			{
+				if (!quiet)
+				{
+					printLine();
+				}
+
+				// Loop over the input files and append their MP3 frames to the output file.
+				foreach (var inpath in inpaths)
+				{
+					if (!quiet)
+					{
+						Console.WriteLine($"+ {inpath}");
+					}
+
+					using (var infile = new FileStream(inpath, FileMode.Open, FileAccess.Read))
+					{
+						var isFirstFrame = true;
+
+						while (true)
+						{
+							// Read the next frame from the input file.
+							var frame = MP3lib.NextFrame(infile);
+							if (frame == null)
+								break;
+
+							// Skip the first frame if it's a VBR header.
+							if (isFirstFrame)
+							{
+								isFirstFrame = false;
+								if (MP3lib.IsXingHeader(frame) || MP3lib.IsVbriHeader(frame))
+									continue;
+							}
+
+							// If we detect more than one bitrate we'll need to add a VBR header to the output file.
+							if (firstBitRate == 0)
+								firstBitRate = frame.BitRate;
+
+							else if (frame.BitRate != firstBitRate)
+								isVBR = true;
+
+							// Write the frame to the output file.
+							outfile.Write(frame.RawBytes, 0, frame.RawBytes.Length);
+
+							totalFrames++;
+							totalBytes += (UInt32)frame.RawBytes.Length;
+						}
+					}
+					totalFiles++;
+				}
+			}
+
+			if (!quiet)
+				printLine();
+
+			// If we detected multiple bitrates, prepend a VBR header to the file.
+			if (isVBR)
+			{
+				if (!quiet)
+					Console.Write("• Multiple bitrates detected. Adding VBR header.");
+				addXingHeader(outpath, totalFrames, totalBytes);
+			}
+
+			// Copy the ID3v2 tag from the n-th input file if requested. Order of operations is important
+			// here. The ID3 tag must be the first item in the file - in particular, it must come *before*
+			// any VBR header.
+			if (tagpath != "")
+			{
+				if (!quiet)
+					Console.WriteLine($"• Copying ID3 tag from: {tagpath}");
+				addID3v2Tag(outpath, tagpath);
+			}
+
+			// Print a count of the number of files merged.
+			if (!quiet)
+			{
+				Console.WriteLine($"• {totalFiles} files merged.");
+				printLine();
+			}
+		}
+
+		// Prepend an Xing VBR header to the specified MP3 file.
+		public static void addXingHeader(string filepath, UInt32 totalFrames, UInt32 totalBytes)
+		{
+			using (var outputFile = new FileStream($"{filepath}.mp3cat.tmp", FileMode.Create, FileAccess.Write))
+			using (var inputFile = new FileStream(filepath, FileMode.Open, FileAccess.Read))
+			{
+				var xingHeader = MP3lib.NewXingHeader(totalFrames, totalBytes);
+				outputFile.Write(xingHeader.RawBytes, 0, xingHeader.RawBytes.Length);
+				inputFile.CopyTo(outputFile);
+			}
+			File.Delete(filepath);
+			File.Move($"{filepath}.mp3cat.tmp", filepath);
+		}
+
+		// Prepend an ID3v2 tag to the MP3 file at mp3Path, copying from tagPath.
+		public static void addID3v2Tag(string mp3Path, string tagPath)
+		{
+			var id3tag = null as ID3v2Tag;
+			using (var tagFile = new FileStream(tagPath, FileMode.Open, FileAccess.Read))
+			{
+				id3tag = MP3lib.NextID3v2Tag(tagFile);
+			}
+
+			if (id3tag != null)
+			{
+				using (var outputFile = new FileStream($"{mp3Path}.mp3cat.tmp", FileMode.Create, FileAccess.Write))
+				using (var inputFile = new FileStream(mp3Path, FileMode.Open, FileAccess.Read))
+				{
+					outputFile.Write(id3tag.RawBytes, 0, id3tag.RawBytes.Length);
+					inputFile.CopyTo(outputFile);
+				}
+				File.Delete(mp3Path);
+				File.Move($"{mp3Path}.mp3cat.tmp", mp3Path);
+			}
+		}
+
+		// Print a line to stdout if we're running in a terminal.
+		public static void printLine()
+		{
+			for (var i = 0; i < Console.WindowWidth; i++)
+				Console.Write("-");
+			Console.WriteLine();
+		}
+	}
+}
---
+++ mp3cat/Program.cs
@@ -0,0 +1,277 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using MP3libSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace mp3cat
+{
+    public class Program
+    {
+        //public const string version = "4.2.2";
+        public const string version = "1.0.0";
+        public static void Main(string[] args)
+        {
+            var wait = false;
+            if (args.Length == 0)
+            {
+                wait = true;
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("MP3cat");
+                Console.ResetColor();
+                Console.Write(" > ");
+
+                var cmd = Console.ReadLine();
+                if (string.IsNullOrEmpty(cmd))
+                    Environment.Exit(0);
+                args = splitArgs(cmd).ToArray();
+            }
+
+            // Parse the command line arguments.
+            var parser = new Parser(with =>
+            {
+                with.HelpWriter = null;
+                with.AutoHelp = false;
+                with.AutoVersion = false;
+            });
+
+            try
+            {
+                var parserResult = parser.ParseArguments<Options>(args);
+                parserResult.WithParsed(o => DoWork(o));
+                Environment.ExitCode = 1;
+            }
+            catch (Exception ex)
+            {
+                Environment.ExitCode = 0;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+
+            if (wait)
+                Console.ReadLine();
+        }
+
+        public static void DoWork(Options options)
+        {
+            if (options.help)
+            {
+                Console.WriteLine(GetHelp());
+                return;
+            }
+            if (options.version)
+            {
+                Console.WriteLine(version);
+                return;
+            }
+
+            // Make sure we have a list of files to merge.
+            var files = null as string[];
+
+            if (options.dir != "")
+            {
+                var temp = new List<string>();
+                var filePaths = Directory.GetFiles(options.dir);
+                foreach (var path in filePaths)
+                {
+                    if (Path.GetExtension(path) == ".mp3")
+                        temp.Add(path);
+                }
+                if (temp.Count == 0)
+                {
+                    throw new Exception("Error: no files found.");
+                }
+                files = temp.ToArray();
+            }
+            else if (options.files.Any())
+            {
+                files = options.files.ToArray();
+            }
+            else
+            {
+                throw new Exception("Error: you must specify files to merge.");
+            }
+
+            // Are we copying the ID3 tag from the n-th input file?
+            var tagpath = "";
+            if (options.meta != 0)
+            {
+                var tagindex = options.meta - 1;
+                if (tagindex < 0 || tagindex > files.Length - 1)
+                    throw new Exception("Error: --meta argument is out of range.");
+                tagpath = files[tagindex];
+            }
+
+            // Are we interlacing a spacer file?
+            if (options.interlace != "")
+            {
+                files = interlace(files, options.interlace);
+            }
+
+            // Make sure all the files in the list actually exist.
+            validateFiles(files);
+
+            // Set debug mode if the user supplied a --debug flag.
+            if (options.debug)
+            {
+                MP3lib.DebugMode = true;
+            }
+
+            // Merge the input files.
+            MP3cat.merge(options._out, tagpath, files, options.force, options.quiet);
+        }
+
+        public static string GetHelp()
+        {
+            var builder = new StringBuilder();
+            builder.AppendLine("Usage: MP3cat [files]");
+            builder.AppendLine("");
+            builder.AppendLine("  This tool concatenates MP3 files without re-encoding. Input files can be");
+            builder.AppendLine("  specified as a list of filenames:");
+            builder.AppendLine("");
+            builder.AppendLine("    $ MP3cat one.mp3 two.mp3 three.mp3");
+            builder.AppendLine("");
+            builder.AppendLine("  Alternatively, an entire directory of .mp3 files can be concatenated:");
+            builder.AppendLine("");
+            builder.AppendLine("    $ MP3cat --dir /path/to/directory");
+            builder.AppendLine("");
+            builder.AppendLine("Arguments:");
+            builder.AppendLine("  [files]                 List of files to merge.");
+            builder.AppendLine("");
+            builder.AppendLine("Options:");
+            builder.AppendLine("  -d, --dir <path>        Directory of files to merge.");
+            builder.AppendLine("  -m, --meta <n>          Copy ID3 metadata from the n-th input file.");
+            builder.AppendLine("  -o, --out <path>        Output filepath. Defaults to 'output.mp3'.");
+            builder.AppendLine("");
+            builder.AppendLine("Flags:");
+            builder.AppendLine("  -f, --force             Overwrite an existing output file.");
+            builder.AppendLine("  -h, --help              Display this help text and exit.");
+            builder.AppendLine("  -q, --quiet             Quiet mode. Only output error messages.");
+            builder.AppendLine("  -v, --version           Display the version number and exit.");
+            return builder.ToString();
+        }
+
+        public class Options
+        {
+            [Value(0, Hidden = true)]
+            public IEnumerable<string> files { get; set; }
+
+            [Option('d', "dir", Default = "")]
+            public string dir { get; set; }
+
+            [Option('m', "meta", Default = 0)]
+            public int meta { get; set; }
+
+            [Option('o', "out", Default = "output.mp3")]
+            public string _out { get; set; }
+
+            [Option('i', "interlace", Default = "")]
+            public string interlace { get; set; }
+
+            [Option('f', "force", Default = false)]
+            public bool force { get; set; }
+
+            [Option('h', "help", Default = false)]
+            public bool help { get; set; }
+
+            [Option('q', "quiet", Default = false)]
+            public bool quiet { get; set; }
+
+            [Option('v', "version", Default = false)]
+            public bool version { get; set; }
+
+            [Option("debug", Default = false)]
+            public bool debug { get; set; }
+        }
+
+        // Check that all the files in the list exist.
+        public static void validateFiles(string[] files)
+        {
+            foreach (var file in files)
+            {
+                if (!File.Exists(file))
+                    throw new Exception($"Error: the file '{file}' does not exist.");
+            }
+        }
+
+        // Interlace a spacer file between each file in the list.
+        public static string[] interlace(string[] files, string spacer)
+        {
+            var interlaced = new List<string>();
+            foreach (var file in files)
+            {
+                interlaced.Add(file);
+                interlaced.Add(spacer);
+            }
+            interlaced.RemoveAt(interlaced.Count - 1);
+            return interlaced.ToArray();
+        }
+
+        private static IEnumerable<string> splitArgs(string commandLine)
+        {
+            var result = new StringBuilder();
+
+            var quoted = false;
+            var escaped = false;
+            var started = false;
+            var allowcaret = false;
+            for (int i = 0; i < commandLine.Length; i++)
+            {
+                var chr = commandLine[i];
+
+                if (chr == '^' && !quoted)
+                {
+                    if (allowcaret)
+                    {
+                        result.Append(chr);
+                        started = true;
+                        escaped = false;
+                        allowcaret = false;
+                    }
+                    else if (i + 1 < commandLine.Length && commandLine[i + 1] == '^')
+                    {
+                        allowcaret = true;
+                    }
+                    else if (i + 1 == commandLine.Length)
+                    {
+                        result.Append(chr);
+                        started = true;
+                        escaped = false;
+                    }
+                }
+                else if (escaped)
+                {
+                    result.Append(chr);
+                    started = true;
+                    escaped = false;
+                }
+                else if (chr == '"')
+                {
+                    quoted = !quoted;
+                    started = true;
+                }
+                else if (chr == '\\' && i + 1 < commandLine.Length && commandLine[i + 1] == '"')
+                {
+                    escaped = true;
+                }
+                else if (chr == ' ' && !quoted)
+                {
+                    if (started) yield return result.ToString();
+                    result.Clear();
+                    started = false;
+                }
+                else
+                {
+                    result.Append(chr);
+                    started = true;
+                }
+            }
+            if (started) yield return result.ToString();
+        }
+    }
+}
